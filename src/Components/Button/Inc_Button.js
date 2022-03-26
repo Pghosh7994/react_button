039a52bd1c5dcfec83ee4331c6a79eaaf5a212dc
Pghosh7994 <priyankapinkighosh@gmail.com>
2022-03-26 16:37:05 +0600

    code optimize

--- src/Components/Button/Inc_Button.js
+++ src/Components/Button/Inc_Button.js
@@ -3,25 +3,22 @@ import './Inc_Button.css'
 const Inc_Button = () => {
 
     const [val, setVal] = useState(0);
-    const incval = () => {
-        setVal(val + 1);
-    }
-    const decval = () => {
-        if (val > 0) {
-            setVal(val - 1);
-        } else {
-            setVal(0);
-            alert('Limit End')
-        }
-    }
 
     return (
         <div className='main'>
             <div className='display'>
                 <h1> {val} </h1>
                 <div>
-                    <button onClick={incval}>Add</button>
-                    <button onClick={decval}>Remove</button>
+                    <button onClick={() => setVal(val + 1)}>Add</button>
+                    <button onClick={() => {
+                        if (val > 0) {
+                            setVal(val - 1);
+                        } else {
+                            setVal(0);
+                            alert('Limit End')
+                        }
+                    }
+                    }>Remove</button>
                 </div>
             </div>
         </div>
